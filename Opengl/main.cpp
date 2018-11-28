ccbfa4721bec101a62c5d6fb3124be1b1946d577
rong05 <281603980@qq.com>
2018-11-28 16:20:11 +0800

    纹理

--- Opengl/main.cpp
+++ Opengl/main.cpp
@@ -1,11 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
-/*#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 
-#define STB_IMAGE_IMPLEMENTATION*/
-//#include "stb_image.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
